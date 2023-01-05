9e38f0f71149b10502e657f6a9de7f422f2b1395
Rynobe <66514626+Rynobe@users.noreply.github.com>
2023-01-05 15:26:35 +0100

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -24,7 +24,7 @@ class Sonarqube:
 
     def set_user_permission(self, user_names: List[str], projects_name: List[str], permission: str):
         print("Start permission settings for user(s).")
-        print(user_names)
+        print("{0}: {1}", permission, user_names)
         user_endpoint_url = "api/permissions/add_user"
         if permission == "ro":
             for access in self.RO_PERMISSION:
@@ -39,7 +39,7 @@ class Sonarqube:
 
     def set_group_permission(self, group_names: List[str], projects_name: List[str], permission: str):
         print("Start permission settings for group(s).")
-        print(group_names)
+        print("{0}: {1}",permission, group_names)
         group_endpoint_url = "api/permissions/add_group"
         if permission == "ro":
             for access in self.RO_PERMISSION:
