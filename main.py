2daf24c6f2ed0d84ca9c90f7b42a0478571e6943
Rynobe <66514626+Rynobe@users.noreply.github.com>
2023-01-05 13:07:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ def set_permission():
         env = []
         env.append(SONARQUBE_ENV_CONFIG['test'])
         for sonarqube_env in env:
+            print(args.sonarqube_auth_token)
+            print(" " + args.sonarqube_auth_token)
             sonarqube = Sonarqube(token=args.sonarqube_auth_token, sonarqube_url=sonarqube_env['url'])
             sonarqube.validate_users_case_insensitive(rw_users + ro_users)
             sonarqube.validate_projects_case_insensitive(projects)
